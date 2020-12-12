1b1e8cda1d37ff27fe824a1b47d42e91e9b43e7d
chezynick <chezynickannie@yahoo.co.uk>
2020-12-12 13:10:41 +0000

    continued with upload form and added handlers and submit

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,20 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Upload = () => {
+	const [course, setCourse] = useState(false);
+	const [newTitle, setNewTitle] = useState('');
+	const [newLink, setNewLink] = useState('');
+	const [newDescription, setNewDescription] = useState('');
+	const [newImage, setNewImage] = useState('');
+	const [newRecipe, setNewRecipe] = useState();
+	//event handlers
+	const clickHandler = () => {
+		setCourse(!course);
+	};
+	const titleHandler = (e) => {
+		setNewTitle(e.target.value);
+	};
+	const descriptionHandler = (e) => {
+		setNewDescription(e.target.value);
+	};
+	const linkHandler = (e) => {
+		setNewLink(e.target.value);
+	};
+	const imageHandler = (e) => {
+		setNewImage(e.target.value);
+	};
+	//submit handler
+	const submitHandler = () => {
+		let newCourse = '';
+		if (course === false) {
+			newCourse = 'mains';
+		} else {
+			newCourse = 'dessert';
+		}
+		setNewRecipe({
+			title: newTitle.toLowerCase(),
+			description: newDescription,
+			link: newLink,
+			image: newImage,
+			course: newCourse,
+		});
+		return newRecipe;
+	};
+	console.log(newRecipe);
 	return (
 		<UploadStyle>
 			<h3>Add your latest Recipe here...</h3>
 			<FormStyle>
 				<h3>Title :</h3>
-				<textarea cols="20" rows="1"></textarea>
+				<textarea cols="20" rows="1" value={newTitle} onChange={titleHandler}></textarea>
 				<h3>Description :</h3>
-				<textarea cols="20" rows="1"></textarea>
+				<textarea cols="20" rows="1" value={newDescription} onChange={descriptionHandler}></textarea>
 				<h3>Link :</h3>
-				<textarea cols="20" rows="1"></textarea>
+				<textarea cols="20" rows="1" value={newLink} onChange={linkHandler}></textarea>
 				<h3>Image link :</h3>
-				<textarea cols="20" rows="1"></textarea>
+				<textarea cols="20" rows="1" value={newImage} onChange={imageHandler}></textarea>
+				{course ? <ButtonTrue>Mains</ButtonTrue> : <ButtonFalse onClick={clickHandler}>Mains</ButtonFalse>}
+				{course ? <ButtonFalse onClick={clickHandler}>Dessert</ButtonFalse> : <ButtonTrue>Dessert</ButtonTrue>}
 			</FormStyle>
+			<ButtonTrue onClick={submitHandler}>Add Recipe</ButtonTrue>
 		</UploadStyle>
 	);
 };
@@ -37,7 +80,27 @@ const FormStyle = styled.div`
 	grid-row-gap: 20px;
 	width: 100%;
 	height: 100%;
-
+	text-align: center;
 	margin: 2%;
 	padding: 1%;
+	textarea {
+		margin: auto;
+		text-align: center;
+		font-size: larger;
+	}
+`;
+const ButtonTrue = styled.button`
+	width: 50%;
+	background-color: #fb1970;
+	color: white;
+	margin: auto;
+	font-size: larger;
+`;
+const ButtonFalse = styled.button`
+	width: 50%;
+	background-color: lightgray;
+	color: #555555;
+	opacity: 0.5;
+	font-size: larger;
+	margin: auto;
 `;
